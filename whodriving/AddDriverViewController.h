337d5ecd7bbe1986e9073bf7f9695496ffb7555f
Josh Freed jf2299@gmail.com <jf2299@gmail.com>
2014-09-19 19:42:56 -0400

    Started implementing storyboards for the Who's Driving?! app. No data yet.

---
+++ whodriving/AddDriverViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddDriverViewController.h
+//  whodriving
+//
+//  Created by Josh Freed on 9/19/14.
+//  Copyright (c) 2014 Josh Freed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddDriverViewController : UIViewController
+
+@end
---
+++ whodriving/AddDriverViewController.m
@@ -0,0 +1,37 @@
+//
+//  AddDriverViewController.m
+//  whodriving
+//
+//  Created by Josh Freed on 9/19/14.
+//  Copyright (c) 2014 Josh Freed. All rights reserved.
+//
+
+#import "AddDriverViewController.h"
+
+@interface AddDriverViewController ()
+
+@end
+
+@implementation AddDriverViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
