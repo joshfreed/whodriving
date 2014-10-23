dcf31030aeb363cb95a4cadb8607d45c66e5e995
Josh Freed jf2299@gmail.com <jf2299@gmail.com>
2014-10-23 13:51:22 -0400

    Fixed trip algorithm to include the driver when determining how many people a car can transport. And if any drivers can accommodate the entire party, randomly select one of them.

--- whodriving/TripService.m
+++ whodriving/TripService.m
@@ -14,6 +14,8 @@
 
 -(NSArray*)buildTrip:(TripSpecification*)tripSpec
 {
+    NSMutableArray *results = [NSMutableArray array];
+    
     NSArray *sortedDrivers = [tripSpec.possibleDrivers sortedArrayUsingComparator:^(id obj1, id obj2) {
         if (rand() % 2 == 0) {
             return NSOrderedAscending;
@@ -21,7 +23,20 @@
             return NSOrderedDescending;
         }
     }];
-    NSMutableArray *results = [NSMutableArray array];
+    
+    NSPredicate *capableDriverPredicate = [NSPredicate predicateWithBlock:^BOOL(Driver* driver, NSDictionary *bindings) {
+        return driver.isEnabled && [driver.numPassengers intValue] + 1 >= [tripSpec.passengerCount intValue];
+    }];
+    NSArray *filteredArray = [sortedDrivers filteredArrayUsingPredicate:capableDriverPredicate];
+    if (filteredArray.count > 0) {
+        Driver *driver = filteredArray.firstObject;
+        DriverResult *result = [[DriverResult alloc] init];
+        result.driver = driver;
+        result.passengerCount = driver.numPassengers;
+        [results addObject:result];
+        return results;
+    }
+    
     int satisfiedPassengers = 0;
     for (Driver *driver in sortedDrivers) {
         if (!driver.isEnabled) {
@@ -33,7 +48,7 @@
         result.passengerCount = driver.numPassengers;
         [results addObject:result];
         
-        satisfiedPassengers += [driver.numPassengers intValue];
+        satisfiedPassengers += [driver.numPassengers intValue] + 1;
         if (satisfiedPassengers >= [tripSpec.passengerCount intValue]) {
             break;
         }
