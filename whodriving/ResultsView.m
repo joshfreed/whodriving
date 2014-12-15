b54d8df13a90c6a3e962640edfe5bd3238f124ac
Josh Freed jf2299@gmail.com <jf2299@gmail.com>
2014-12-15 18:54:40 -0500

    Center the search form and results container above their respective buttons instead of centering them in the parent view

--- whodriving/ResultsView.m
+++ whodriving/ResultsView.m
@@ -13,7 +13,7 @@
 
 @interface ResultsView ()
 @property (weak, nonatomic) IBOutlet UIButton *doneButton;
-@property NSArray *driverLabels;
+@property (weak, nonatomic) IBOutlet UIView *driverNamesContainer;
 @end
 
 @implementation ResultsView
@@ -21,7 +21,6 @@
 -(void)awakeFromNib
 {
     [self setTranslatesAutoresizingMaskIntoConstraints:NO];
-    self.driverLabels = [NSArray array];
 }
 
 -(void)viewWillAppear
@@ -68,34 +67,34 @@
 
 -(void)displayDrivers:(NSArray*)drivingDrivers
 {
-    [self.driverLabels makeObjectsPerformSelector:@selector(removeFromSuperview)];
+    [[self.driverNamesContainer subviews] makeObjectsPerformSelector:@selector(removeFromSuperview)];
     
     int i = 0;
     
     UIView *spacer1 = [UIView new];
     spacer1.translatesAutoresizingMaskIntoConstraints = NO;
     //    spacer1.backgroundColor = [UIColor grayColor];
-    [self addSubview:spacer1];
+    [self.driverNamesContainer addSubview:spacer1];
     
     UIView *spacer2 = [UIView new];
     spacer2.translatesAutoresizingMaskIntoConstraints = NO;
     //    spacer2.backgroundColor = [UIColor brownColor];
-    [self addSubview:spacer2];
+    [self.driverNamesContainer addSubview:spacer2];
     
-    [self.superview addConstraint:[NSLayoutConstraint constraintWithItem:spacer1
-                                                                 attribute:NSLayoutAttributeCenterX
-                                                                 relatedBy:NSLayoutRelationEqual
-                                                                    toItem:self.superview
-                                                                 attribute:NSLayoutAttributeCenterX
-                                                                multiplier:1
-                                                                  constant:0]];
-    [self.superview addConstraint:[NSLayoutConstraint constraintWithItem:spacer2
-                                                                 attribute:NSLayoutAttributeCenterX
-                                                                 relatedBy:NSLayoutRelationEqual
-                                                                    toItem:self.superview
-                                                                 attribute:NSLayoutAttributeCenterX
-                                                                multiplier:1
-                                                                  constant:0]];
+    [self addConstraint:[NSLayoutConstraint constraintWithItem:spacer1
+                                                     attribute:NSLayoutAttributeCenterX
+                                                     relatedBy:NSLayoutRelationEqual
+                                                        toItem:self.driverNamesContainer
+                                                     attribute:NSLayoutAttributeCenterX
+                                                    multiplier:1
+                                                      constant:0]];
+    [self addConstraint:[NSLayoutConstraint constraintWithItem:spacer2
+                                                     attribute:NSLayoutAttributeCenterX
+                                                     relatedBy:NSLayoutRelationEqual
+                                                        toItem:self.driverNamesContainer
+                                                     attribute:NSLayoutAttributeCenterX
+                                                    multiplier:1
+                                                      constant:0]];
     [spacer1 addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:[spacer1(10)]"
                                                                     options:0
                                                                     metrics:nil
@@ -116,7 +115,7 @@
         [driverLabel setMinimumScaleFactor:0.1];
         [driverLabel setAdjustsFontSizeToFitWidth:YES];
         [driverLabel setBaselineAdjustment:UIBaselineAdjustmentAlignCenters];
-        [self addSubview:driverLabel];
+        [self.driverNamesContainer addSubview:driverLabel];
         [labels addObject:driverLabel];
     }
     
@@ -144,10 +143,10 @@
         i++;
     }
     [vFormat appendString:@"[spacer2(==spacer1)]|"];
-    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:vFormat
-                                                                 options:0
-                                                                 metrics:nil
-                                                                   views:viewsDictionary]];
+    [self.driverNamesContainer addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:vFormat
+                                                                                      options:0
+                                                                                      metrics:nil
+                                                                                        views:viewsDictionary]];
     
     i = 0;
     for (UILabel *driverLabel in labels) {
@@ -157,26 +156,19 @@
                                                                             options:0
                                                                             metrics:nil
                                                                               views:viewsDictionary]];
-        [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:[NSString stringWithFormat:@"H:|-[%@]-|", viewName]
-                                                                                 options:0
-                                                                                 metrics:nil
-                                                                                   views:viewsDictionary] ];
-        [self addConstraint:[NSLayoutConstraint constraintWithItem:driverLabel
-                                                         attribute:NSLayoutAttributeCenterX
-                                                         relatedBy:NSLayoutRelationEqual
-                                                            toItem:self
-                                                         attribute:NSLayoutAttributeCenterX
-                                                        multiplier:1
-                                                          constant:0]];
+        [self.driverNamesContainer addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:[NSString stringWithFormat:@"H:|-[%@]-|", viewName]
+                                                                                          options:0
+                                                                                          metrics:nil
+                                                                                            views:viewsDictionary]];
+        [self.driverNamesContainer addConstraint:[NSLayoutConstraint constraintWithItem:driverLabel
+                                                                              attribute:NSLayoutAttributeCenterX
+                                                                              relatedBy:NSLayoutRelationEqual
+                                                                                 toItem:self.driverNamesContainer
+                                                                              attribute:NSLayoutAttributeCenterX
+                                                                             multiplier:1
+                                                                               constant:0]];
         i++;
     }
-    
-    self.driverLabels = labels;
-    
-//    for (UILabel *driverLabel in labels) {
-//        NSLog(@"Intrinsic: %f, %f  Size: %f, %f", driverLabel.intrinsicContentSize.width, driverLabel.intrinsicContentSize.height, driverLabel.frame.size.width, driverLabel.frame.size.height);
-//    }
-
 }
 
 - (IBAction)doneButton:(UIButton *)sender
